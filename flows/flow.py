4207f6f5859909d6354657c81e0978505826f30c
Mark <marcoelumba@gmail.com>
2022-07-07 18:14:03 +0300

    Update flow.py

--- flows/flow.py
+++ flows/flow.py
@@ -9,7 +9,7 @@ def hello_task():
     logger = prefect.context.get("logger")
     logger.info("Hello world!")
 
-with Flow("my-first-flow",  schedule=schedule) as flow:
+with Flow("my-first-flow") as flow:
     hello_task()
 
 flow.storage = GitHub(
